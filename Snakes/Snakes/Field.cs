5b2beb61d79027dd330699e41414640a7fa24a60
thesuperfalcon <tintinfalk01@gmail.com>
2023-10-11 12:02:07 +0200

    First commit

---
+++ Snakes/Snakes/Field.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snakes
+{
+    internal class Field
+    {
+        public string[] Fields { get; set; }
+    }
+}
